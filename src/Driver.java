564dfbeb0ba6958e476da9dfb2137ce2667d38bd
Khawaja Shaheryar <58368527+RNGesus-exe@users.noreply.github.com>
2020-06-25 20:50:00 +0500

    Added Spam and All Mail + DataBase improvement.

--- src/Driver.java
+++ src/Driver.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.sql.Connection;
+import java.sql.SQLException;
 
 
 public class Driver {
@@ -7,11 +8,12 @@ public class Driver {
     public static Mail mail = new Mail();                  //Handles all Users Data in RAM
     public static DatabaseManager dataAgent = null;        //This agent helps in managing database
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws SQLException {
         Connection connectionAgent = ConnectionManager.getConnection();  //Connects to MySql through Xampp
         if (connectionAgent != null) {
             dataAgent = new DatabaseManager(connectionAgent);
-            new MainMenu();
+            //new MainMenu();
+            dataAgent.deleteMail(5);
         } else {   //In case Connection is not established
             JOptionPane.showMessageDialog(null, "Connection was not established!", "Database connection Error", JOptionPane.WARNING_MESSAGE);
         }
